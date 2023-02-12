8a7642a0235ef04a841cbcf12d68784e3e30934a
ShashankSaumya123 <shashank.saumya@niser.ac.in>
2023-02-12 21:00:15 +0530

    small camera changes

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,10 +4,46 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    // The camera should follow the player
     [SerializeField] private Transform player;
-    void Update()
+
+	[SerializeField] private float sideBoundaryLength = 10f;
+
+    [SerializeField] private float shift = 8f;
+
+    [SerializeField] private float damping = 5f;
+
+	void Start ()
     {
-        // Follows the player in both axes
-        // transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
-    }
+
+		// the map MinX and MinY are the position that the camera STARTS
+		// minX = transform.position.x;
+		// minY = transform.position.y;
+        // minZ = transform.position.z;
+		// // the desired max boundaries
+		// maxX = mapX;
+		// maxY = mapY;
+	}
+
+	void Update ()
+    {
+
+        Vector3 wantedPosition = transform.position;
+
+        float rightBoundary = transform.position.x + sideBoundaryLength;
+        float leftBoundary = transform.position.x - sideBoundaryLength;
+
+		if (player.transform.position.x>rightBoundary)
+        {
+            wantedPosition.x += shift;
+        }
+
+        else if (player.transform.position.x<leftBoundary)
+        {
+            wantedPosition.x -= shift;
+        }
+
+        // set the camera to go to the wanted position in a certain amount of time
+        transform.position = Vector3.Lerp (transform.position, wantedPosition, (Time.deltaTime * damping));
+	}
 }
